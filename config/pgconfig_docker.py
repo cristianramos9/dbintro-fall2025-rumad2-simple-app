1fed7d8bc24f6f941b5b920e6fa69c01ac1a8bc8
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-11-08 23:53:16 -0400

    Added file with credentials for database in Docker.

---
+++ config/pgconfig_docker.py
@@ -0,0 +1,7 @@
+pg_config = {
+        "dbname"    :   "rumad2_fase2",
+        "user"      :   "cramos",
+        "password"  :   "uprmrumad2",
+        "host"      :   "localhost",
+        "port"      :   "5432"
+        }
