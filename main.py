0077a7db860ed47f39d50bf3dc15c3eb62f45362
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-11-09 00:06:35 -0400

    [misc] Added test default route.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# test route. delete before deployment.
+@app.route('/')
+def default():
+    message = { "Default route" : "test." }
+    
+    return jsonify(message)
 
 
 if __name__ == '__main__':
