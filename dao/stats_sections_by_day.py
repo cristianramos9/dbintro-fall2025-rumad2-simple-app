fa8f54817eb790613220f348d2ecdf81c135a25a
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-11-11 02:48:43 -0400

    stats: Commented unnecessary functions in handler and dao.

--- dao/stats_sections_by_day.py
+++ dao/stats_sections_by_day.py
@@ -36,116 +36,101 @@ class SectionsByDayDAO:
             cursor.close()
 
 
-    def getSectionCount(self):
-        cursor = self.conn.cursor()
-        query_l = ("L", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%L%'")
-        query_m = ("M", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%M%'")
-        query_w = ("W", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%W%'")
-        query_j = ("J", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%J%'")
-        query_v = ("V", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%V%'")
-        query_s = ("S", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%S%'")
-        query_d = ("D", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%D%'")
-
-        query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
-        result = []
-        rday = {}
-
-        for query in query_list:
-            cursor.execute(query[1])
-            rday[query[0]] = cursor.fetchone()[0]
-            result.append(rday[query[0]])
-            
-        return rday
-
-
-    def getSectionCountUsingYear(self, year):
+#   def getSectionCount(self):
 #       cursor = self.conn.cursor()
-        year = "'" + year + "'"
+#       query_l = ("L", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%L%'")
+#       query_m = ("M", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%M%'")
+#       query_w = ("W", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%W%'")
+#       query_j = ("J", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%J%'")
+#       query_v = ("V", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%V%'")
+#       query_s = ("S", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%S%'")
+#       query_d = ("D", "SELECT COUNT(cdays) FROM section NATURAL INNER JOIN meeting WHERE cdays LIKE '%D%'")
+#       query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
+#       result = []
+#       rday = {}
+#       for query in query_list:
+#           cursor.execute(query[1])
+#           rday[query[0]] = cursor.fetchone()[0]
+#           result.append(rday[query[0]])
+#       return rday
+
+
+#   def getSectionCountUsingYear(self, year):
+#       cursor = self.conn.cursor()
+#       year = "'" + year + "'"
 #       print(debug,"before query:", year, type(year))
-        query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%L%'"))
-        query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%M%'"))
-        query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%W%'"))
-        query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%J%'"))
-        query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%V%'"))
-        query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%S%'"))
-        query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%D%'"))
-
-        query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
+#       query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%L%'"))
+#       query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%M%'"))
+#       query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%W%'"))
+#       query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%J%'"))
+#       query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%V%'"))
+#       query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%S%'"))
+#       query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s) WHERE cdays LIKE %s" % (str(year), "'%D%'"))
+#       query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
 #       query_list = [query_l]
-        result = []
-        rday = {}
+#       result = []
+#       rday = {}
 #       print(debug, "query list:", query_list)
-        
-
-        for query in query_list:
-            cursor = self.conn.cursor()
+#       for query in query_list:
+#           cursor = self.conn.cursor()
 #           print(debug, "query:", query)
-            cursor.execute(query[1])
-            rday[query[0]] = cursor.fetchone()[0]
-            result.append(rday[query[0]])
+#           cursor.execute(query[1])
+#           rday[query[0]] = cursor.fetchone()[0]
+#           result.append(rday[query[0]])
 #           print(debug, "after cursor.execute:", year, rday)
-            cursor.close()
+#           cursor.close()
 #       print(debug, "after query:", year, "type:", type(year))
-            
-        return rday
+#       return rday
 
 
-    def getSectionCountUsingSemester(self, semester):
+#   def getSectionCountUsingSemester(self, semester):
 #       cursor = self.conn.cursor()
-        semester = "'" + semester + "'"
+#       semester = "'" + semester + "'"
 #       print(debug,"before query:", semester, type(semester))
-        query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%L%'"))
-        query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%M%'"))
-        query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%W%'"))
-        query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%J%'"))
-        query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%V%'"))
-        query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%S%'"))
-        query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%D%'"))
-
-        query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
-        result = []
-        rday = {}
+#       query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%L%'"))
+#       query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%M%'"))
+#       query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%W%'"))
+#       query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%J%'"))
+#       query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%V%'"))
+#       query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%S%'"))
+#       query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE semester LIKE %s) WHERE cdays LIKE %s" % (str(semester), "'%D%'"))
+#       query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
+#       result = []
+#       rday = {}
 #       print(debug, "query list:", query_list)
-        
-
-        for query in query_list:
-            cursor = self.conn.cursor()
+#       for query in query_list:
+#           cursor = self.conn.cursor()
 #           print(debug, "query:", query)
-            cursor.execute(query[1])
-            rday[query[0]] = cursor.fetchone()[0]
-            result.append(rday[query[0]])
+#           cursor.execute(query[1])
+#           rday[query[0]] = cursor.fetchone()[0]
+#           result.append(rday[query[0]])
 #           print(debug, "after cursor.execute:", semester, rday)
-            cursor.close()
+#           cursor.close()
 #       print(debug, "after query:", semester, "type:", type(semester))
-            
-        return rday
+#       return rday
 
 
-    def getSectionCountUsingYearSemester(self, year, semester):
-        year = "'" + year + "'"
-        semester = "'" + semester + "'"
-        query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%L%'"))
-        query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%M%'"))
-        query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%W%'"))
-        query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%J%'"))
-        query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%V%'"))
-        query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%S%'"))
-        query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%D%'"))
-
-        query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
-        result = []
-        rday = {}
-
-        for query in query_list:
-            cursor = self.conn.cursor()
-            cursor.execute(query[1])
-            rday[query[0]] = cursor.fetchone()[0]
-            result.append(rday[query[0]])
-            cursor.close()
-
+#   def getSectionCountUsingYearSemester(self, year, semester):
+#       year = "'" + year + "'"
+#       semester = "'" + semester + "'"
+#       query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%L%'"))
+#       query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%M%'"))
+#       query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%W%'"))
+#       query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%J%'"))
+#       query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%V%'"))
+#       query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%S%'"))
+#       query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%D%'"))
+#       query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
+#       result = []
+#       rday = {}
+#       for query in query_list:
+#           cursor = self.conn.cursor()
+#           cursor.execute(query[1])
+#           rday[query[0]] = cursor.fetchone()[0]
+#           result.append(rday[query[0]])
+#           cursor.close()
 #       print(debug, "year & semester", result)
-            
-        return rday
+#       return rday
 
 
     def getSectionCountUsingParameter(self, year, semester):
@@ -161,14 +146,6 @@ class SectionsByDayDAO:
         days_sub = ["'%L%'", "'%M%'", "'%W%'", "'%J%'", "'%V%'", "'%S%'", "'%D%'", ]
         print("days:", days_sub[6][2])
 
-        query_l = ("L", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE (%s IS NULL OR years LIKE %s) AND (%s IS NULL OR semester LIKE %s)) WHERE cdays LIKE %s" % (str(year), year, str(semester), str(semester), "'%L%'"))
-        query_m = ("M", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%M%'"))
-        query_w = ("W", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%W%'"))
-        query_j = ("J", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%J%'"))
-        query_v = ("V", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%V%'"))
-        query_s = ("S", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%S%'"))
-        query_d = ("D", "SELECT COUNT(cdays) FROM (SELECT * FROM section NATURAL INNER JOIN meeting WHERE years LIKE %s AND semester LIKE %s) WHERE cdays LIKE %s" % (str(year), str(semester), "'%D%'"))
-
         result = []
         rday = {}
 
@@ -181,7 +158,6 @@ class SectionsByDayDAO:
             cursor.close()
 
 #       query_list = [query_l, query_m, query_w, query_j, query_v, query_s, query_d]
-        query_list = [query_l]
 
 #       for query in query_list:
 #           cursor = self.conn.cursor()
--- handler/stats_sections_by_day.py
+++ handler/stats_sections_by_day.py
@@ -6,86 +6,68 @@ debug = "DEBUG:"
 
 class SectionsByDayHandler:
     semester_list = ("Fall", "Spring", "V1", "V2")
-    def getSectionsByDay(self):
-        dao = SectionsByDayDAO()
-
-        days = ['L','M','W','J','V','S','D']
-        result_list = []
-        
-        result = dao.getSectionCount()
-        print(result)
-
-        for day in days:
-            wday = { "day" : day, "sections" : result[day] }
-            result_list.append(wday)
-            print(debug, day)
-        return jsonify(result_list)
+#   def getSectionsByDay(self):
+#       dao = SectionsByDayDAO()
+#       days = ['L','M','W','J','V','S','D']
+#       result_list = []
+#       result = dao.getSectionCount()
+#       print(result)
+#       for day in days:
+#           wday = { "day" : day, "sections" : result[day] }
+#           result_list.append(wday)
+#           print(debug, day)
+#       return jsonify(result_list)
 
     
-    def getSectionsByDayUsingYear(self, year):
-        # validate year
-        print("year a number?", year.isnumeric())
-        print("year 4 digit?", len(year))
-        print("year 4 digit?", len(year) == 4)
-        if len(year) != 4 or not year.isnumeric():
-            return jsonify(Error = "Bad Request"), 400
-
-        dao = SectionsByDayDAO()
-
-        days = ['L','M','W','J','V','S','D']
-        result_list = []
-        
-        result = dao.getSectionCountUsingYear(year)
-        print(debug, result, "type:", type(result))
-
-        for day in days:
-            wday = { "day" : day, "sections" : result[day] }
-            result_list.append(wday)
-
-        return jsonify(result_list)
+#   def getSectionsByDayUsingYear(self, year):
+#       # validate year
+#       print("year a number?", year.isnumeric())
+#       print("year 4 digit?", len(year))
+#       print("year 4 digit?", len(year) == 4)
+#       if len(year) != 4 or not year.isnumeric():
+#           return jsonify(Error = "Bad Request"), 400
+#       dao = SectionsByDayDAO()
+#       days = ['L','M','W','J','V','S','D']
+#       result_list = []
+#       result = dao.getSectionCountUsingYear(year)
+#       print(debug, result, "type:", type(result))
+#       for day in days:
+#           wday = { "day" : day, "sections" : result[day] }
+#           result_list.append(wday)
+#       return jsonify(result_list)
 
 
-    def getSectionsByDayUsingSemester(self, semester):
-        if semester not in self.semester_list:
-            return jsonify(Error = "Bad Request"), 400
-
-        dao = SectionsByDayDAO()
-
-        days = ['L','M','W','J','V','S','D']
-        result_list = []
-        
-        result = dao.getSectionCountUsingSemester(semester)
-        print(debug, result, "type:", type(result))
-
-        for day in days:
-            wday = { "day" : day, "sections" : result[day] }
-            result_list.append(wday)
-
-        return jsonify(result_list)
+#   def getSectionsByDayUsingSemester(self, semester):
+#       if semester not in self.semester_list:
+#           return jsonify(Error = "Bad Request"), 400
+#       dao = SectionsByDayDAO()
+#       days = ['L','M','W','J','V','S','D']
+#       result_list = []
+#       result = dao.getSectionCountUsingSemester(semester)
+#       print(debug, result, "type:", type(result))
+#       for day in days:
+#           wday = { "day" : day, "sections" : result[day] }
+#           result_list.append(wday)
+#       return jsonify(result_list)
     
 
-    def getSectionsByDayUsingYearSemester(self, year, semester):
-        if len(year) != 4 or not year.isnumeric():
-            return jsonify(Error = "Bad Request"), 400
-        if semester not in self.semester_list:
-            return jsonify(Error = "Bad Request"), 400
+#   def getSectionsByDayUsingYearSemester(self, year, semester):
+#       if len(year) != 4 or not year.isnumeric():
+#           return jsonify(Error = "Bad Request"), 400
+#       if semester not in self.semester_list:
+#           return jsonify(Error = "Bad Request"), 400
+#       dao = SectionsByDayDAO()
+#       days = ['L','M','W','J','V','S','D']
+#       result_list = []
+#       result = dao.getSectionCountUsingYearSemester(year, semester)
+#       print(debug, result, "type:", type(result))
+#       for day in days:
+#           wday = { "day" : day, "sections" : result[day] }
+#           result_list.append(wday)
+#       return jsonify(result_list)
 
 
-        dao = SectionsByDayDAO()
-
-        days = ['L','M','W','J','V','S','D']
-        result_list = []
-        
-        result = dao.getSectionCountUsingYearSemester(year, semester)
-        print(debug, result, "type:", type(result))
-
-        for day in days:
-            wday = { "day" : day, "sections" : result[day] }
-            result_list.append(wday)
-
-        return jsonify(result_list)
-
-
+    # gets count with or without query parameters
     def getSectionsByDayUsingParameter(self, year=None, semester=None):
         if year != None:
             if len(year) != 4 or not year.isnumeric():
